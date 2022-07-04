037192cc78e118f4a6316d50100e31f8072f4fe8
mdarocha <git@mdarocha.pl>
2022-07-04 15:09:43 +0200

    Test sshclient impl

--- src/Docker.DotNet.SSH/SSHCredentials.cs
+++ src/Docker.DotNet.SSH/SSHCredentials.cs
@@ -1,7 +1,9 @@
 using System.IO;
 using System.Net.Http;
 using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.Net.Http.Client;
+using Renci.SshNet;
 
 namespace Docker.DotNet.SSH
 {
@@ -26,7 +28,17 @@ namespace Docker.DotNet.SSH
         {
             // TODO
             return async (string host, int port, CancellationToken cancellationToken) => {
-                return File.OpenWrite("testStream");
+                // TODO username
+                var authMethod = new PrivateKeyAuthenticationMethod("ubuntu", new PrivateKeyFile("/home/marek/test_rsa"));
+                var connectionInfo = new ConnectionInfo(host, port, "ubuntu", authMethod);
+                var client = new SshClient(connectionInfo);
+                client.Connect();
+
+                var shellStream = client.CreateShellStream("dumb-terminal", 1, 1, 1, 1, 50);
+                shellStream.WriteLine("docker system dial-stdio");
+
+                await Task.Delay(100);
+                return shellStream;
             };
         }
     }
